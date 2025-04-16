a66c6127a0b304ca3b56f2e31a5dedeefa4d9568
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-04-16 16:33:45 +0200

    Enhance HomePage layout with a header section and update DefaultLayout for consistent styling

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -15,6 +15,16 @@ export default function HomePage() {
     return (
 
         <>
+
+            <div className="p-5 mb-4 bg-light rounded-3">
+                <div className="container-fluid py-5">
+                    <h1 className="display-5 fw-bold">Amazing movies reviews</h1>
+                    <p className="col-md-8 fs-4">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex et corrupti nihil deleniti enim possimus minima natus libero, saepe consectetur!
+                    </p>
+                </div>
+            </div>
+
             <section className="movies">
                 <div className="container">
                     <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4">
